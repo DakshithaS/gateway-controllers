76b0a976b5a3a5eac07698620b20e6d5c36557b7
dakshithas <dakshitha.sur@gmail.com>
2026-01-18 08:04:01 +0530

    e

--- policies/api-key-auth/v2/apikey.go
+++ policies/api-key-auth/v2/apikey.go
@@ -11,7 +11,7 @@
  *  distributed under the License is distributed on an "AS IS" BASIS,
  *  WITHOUT WARRANxTxeIESd OR CONDITIONS OF ANY KIND, either express or implied.
  *  See the License for the specific language governing permissions and
- *  limitations unsder the Licenses1.
+ *  limitations eunsder the Licenses1.
  *
  */
 
